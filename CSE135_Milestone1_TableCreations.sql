d47c39798318ada2114edd97f878bb0fb8652139
allengong <allengong@137.110.57.0>
2014-05-02 18:39:09 -0700

    removed drops and any uneeded code

--- CSE135_Milestone1_TableCreations.sql
+++ CSE135_Milestone1_TableCreations.sql
@@ -1,6 +1,3 @@
-/* Papa Yannis's syntax */
-
-DROP TABLE IF EXISTS Users CASCADE;
 CREATE TABLE Users
 (
    user_id    SERIAL     PRIMARY KEY,
@@ -11,20 +8,17 @@ CREATE TABLE Users
    CHECK          (name <> '')
 );
 
-DROP TABLE IF EXISTS States CASCADE;
 CREATE TABLE States
 (
     state_id      VARCHAR(2)     PRIMARY KEY,
     state         VARCHAR(20)
 );
 
-DROP TABLE IF EXISTS Roles CASCADE;
 CREATE TABLE Roles
 (
     role          VARCHAR(8)
 );
 
-DROP TABLE IF EXISTS Categories CASCADE;
 CREATE TABLE Categories
 (
     category_id   SERIAL     PRIMARY KEY,
@@ -33,19 +27,17 @@ CREATE TABLE Categories
     CHECK         (name <> '')
 );
 
-DROP TABLE IF EXISTS Products CASCADE;
 CREATE TABLE Products
 (
     product_id    SERIAL     PRIMARY KEY,
     name          VARCHAR(30) 	 NOT NULL,
     sku           VARCHAR(10)    NOT NULL UNIQUE,
     category      INTEGER        REFERENCES Categories(category_id) ON UPDATE CASCADE,
-    price         FLOAT,
+    price         FLOAT 		 NOT NULL,
     CHECK         (sku <> ''),
     CHECK         (price >= 0)
 );
 
-DROP TABLE IF EXISTS Shopping_Cart CASCADE;
 CREATE TABLE Shopping_Cart
 (
     customer_name INTEGER    REFERENCES Users (user_id),
@@ -53,8 +45,6 @@ CREATE TABLE Shopping_Cart
     quantity      INTEGER
 );
 
-/* PRODUCT_ORDER CHANGED NAME TO SHOPPING_CART
-     ALSO ADDED PRODUCT_PRICE ATTR */
 
 
 
@@ -64,58 +54,4 @@ CREATE TABLE Shopping_Cart
 
 
 
-/* Conventional Postgres way */
-/*
-DROP TABLE IF EXISTS Users CASCADE;
-CREATE TABLE Users
-(
-    name          varchar(100),
-    role          varchar(8),
-    age           integer,
-    state         varchar(25),
-    PRIMARY KEY   (name)
-);
 
-DROP TABLE IF EXISTS States CASCADE;
-CREATE TABLE States
-(
-    state_id      varchar(2)
-    state         varchar(100) 
-);
-
-DROP TABLE IF EXISTS Roles CASCADE;
-CREATE TABLE Roles
-(
-    role          varchar(8)
-);
-
-DROP TABLE IF EXISTS Categories CASCADE;
-CREATE TABLE Categories
-(
-    name          varchar(20),
-    description   varchar(100),
-    PRIMARY KEY   (name)
-);
-
-DROP TABLE IF EXISTS Products CASCADE;
-CREATE TABLE Products
-(
-    name          varchar(20),
-    sku           varchar(20),
-    category      varchar(20),
-    price         integer,
-    PRIMARY KEY   (sku),
-    FOREIGN KEY   (category)      REFERENCES Categories(name),
-    CHECK         (price >= 0)
-);
-
-DROP TABLE IF EXISTS Product_Order CASCADE;
-CREATE TABLE Product_Order
-(
-    customer_name varchar(20),
-    product_sku   varchar(20),
-    quantity      integer,
-    FOREIGN KEY   (customer_name) REFERENCES Users(name),
-    FOREIGN KEY   (product_sku)   REFERENCES Products(sku)
-);
-*/
