625192e28fe3de311fb5962c885ebc78ed08f36f
Jasmine Nguyen <jasmine.nguyen@mac.com>
2014-06-05 22:45:31 -0700

    New CREATE TABLEs for precompproductsrow precompstacuscol

--- SQL/Milestone 3/PreComp Tables.sql
+++ SQL/Milestone 3/PreComp Tables.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS PreCompProductsRow; 
+/*DROP TABLE IF EXISTS PreCompProductsRow; 
 CREATE TABLE PreCompProductsRow AS 
 (
     SELECT SUM(sales.quantity*sales.price) AS total, 
@@ -18,10 +18,48 @@ CREATE TABLE PreCompStaCusCol AS
     FROM (sales INNER JOIN users ON sales.uid = users.id) 
          RIGHT OUTER JOIN products ON products.id = sales.pid
     GROUP BY users.state, users.name, products.cid 
+);*/
+
+
+DROP TABLE IF EXISTS PreCompProductsRow CASCADE; 
+CREATE TABLE PreCompProductsRow AS 
+(
+    SELECT total, 
+           products.name,
+           foo.state, 
+           products.cid
+           
+    FROM (
+    SELECT SUM(sales.quantity*sales.price) AS total, users.state, users.id, sales.pid
+    FROM users, sales
+    WHERE sales.uid = users.id
+    GROUP BY users.state, users.id, sales.pid) AS foo
+    RIGHT OUTER JOIN products 
+    ON products.id = foo.pid
+    GROUP BY total, foo.state, products.name, products.cid
 );
 
 
-DROP TABLE IF EXISTS PreCompCells; 
+DROP TABLE IF EXISTS PreCompStaCusCol CASCADE; 
+CREATE TABLE PreCompStaCusCol AS
+(
+    SELECT total, 
+           users.name,
+           users.state, 
+           foo.cid,
+           foo.id
+           
+    FROM (
+    SELECT SUM(sales.quantity*sales.price) AS total, sales.uid, products.cid, sales.id
+    FROM products, sales
+    WHERE sales.pid = products.id
+    GROUP BY sales.uid, products.cid, sales.id) AS foo
+    RIGHT OUTER JOIN users 
+    ON users.id = foo.uid
+    GROUP BY total, users.state, users.name, foo.cid, foo.id
+);
+
+DROP TABLE IF EXISTS PreCompCells CASCADE; 
 CREATE TABLE PreCompCells AS
 (
     SELECT SUM(quantity* sales.price) AS total, products.name, users.state, users.name AS nam
